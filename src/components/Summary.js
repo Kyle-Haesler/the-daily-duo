90d3ee794cb91473ade61097dfa116ab440a825f
Kyle Haesler <kyle.haesler@gmail.com>
2023-12-21 13:13:43 -0500

    summary page set up, need to write in text

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,5 +1,30 @@
+import "./Summary.css";
 function Summary() {
-  return <h2>Summary</h2>;
+  return (
+    <article>
+      <div className="col">
+        <h2>The Daily Duo</h2>
+        <ul>
+          <li>Set out to be the best</li>
+          <li>Set out to be the best</li>
+        </ul>
+      </div>
+      <div className="col">
+        <h2>Summary</h2>
+        <ul>
+          <li>Set out to be the best</li>
+          <li>Set out to be the best</li>
+        </ul>
+      </div>
+      <div className="col">
+        <h2>Who is this for?</h2>
+        <ul>
+          <li>Set out to be the best</li>
+          <li>Set out to be the best</li>
+        </ul>
+      </div>
+    </article>
+  );
 }
 
 export default Summary;
