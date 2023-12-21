9e9c47939cfeed70a17cc228422401f5b7bda214
Kyle Haesler <kyle.haesler@gmail.com>
2023-12-21 16:58:03 -0500

    Adding more diet information

--- src/data/diets.js
+++ src/data/diets.js
@@ -15,6 +15,18 @@ const diets = [
     diet: "No traditional bread",
     tip: "Try a gluten free alternative or just skip it for the day altogether",
   },
+  {
+    diet: "No fast food",
+    tip: "Chipotle is fine :)",
+  },
+  {
+    diet: "No breakfast today",
+    tip: "Drink a lot of water in the morning and hold out until lunch",
+  },
+  {
+    diet: "No snacking today",
+    tip: "Just eat 3 regular meals with no snacks in between",
+  },
 ];
 
 export default diets;
