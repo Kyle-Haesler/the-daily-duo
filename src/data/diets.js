42df6793e8822dd92663732b100d5d002d7ba751
Kyle Haesler <kyle.haesler@gmail.com>
2023-12-21 11:55:29 -0500

    Added diets and workout data files

---
+++ src/data/diets.js
@@ -0,0 +1,16 @@
+const diets = [
+  {
+    diet: "Do not consume added sugar",
+    tip: "Look at nutritition labels and look for added sugar. Avoid these foods!",
+  },
+  {
+    diet: "No alcohol",
+    tip: "If you have a big event tonight, regenerate a new dietary restriction:) ",
+  },
+  {
+    diet: "No normal sodas",
+    tip: "If you regularly drink soda, try drinking the diet version of your favorite beverage",
+  },
+];
+
+export default diets;
---
+++ src/data/workouts.js
@@ -0,0 +1,12 @@
+const workouts = [
+  {
+    workout: "Walk for 30 minutes",
+    tip: "Try completing this in the morning to get it out of the way for the day!",
+  },
+  {
+    workout: "20 push-ups",
+    tip: "These do not have to be consecutive, 20 total. If you cannot perform one push-up, try doing knee push-ups or push-ups on an elevated surface instead",
+  },
+];
+
+export default workouts;
