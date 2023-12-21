59496725676d6f4f7d1f2eb6b21f0d8bd8171e30
Kyle Haesler <kyle.haesler@gmail.com>
2023-12-21 13:32:02 -0500

    Summary component complete, working on generationPage

--- src/components/PlanGenerator.js
+++ src/components/PlanGenerator.js
@@ -1,5 +1,18 @@
+import "./PlanGenerator.css";
+
 function PlanGenerator() {
-  return <h1>Plan Generator</h1>;
+  return (
+    <main>
+      <div className="col">
+        <button>Generate Diet</button>
+        <p>Hi!</p>
+      </div>
+      <div className="col">
+        <button>Generate Workout</button>
+        <p>Hi!</p>
+      </div>
+    </main>
+  );
 }
 
 export default PlanGenerator;
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,22 +5,41 @@ function Summary() {
       <div className="col">
         <h2>The Daily Duo</h2>
         <ul>
-          <li>Set out to be the best</li>
-          <li>Set out to be the best</li>
+          <li>
+            The Daily Duo was created to inspire small, consistent changes
+          </li>
+          <li>
+            We believe that small, relatively easy daily changes can lead to
+            significant positive effects down the road
+          </li>
+          <li>We believe in the power of consistency</li>
         </ul>
       </div>
       <div className="col">
         <h2>Summary</h2>
         <ul>
-          <li>Set out to be the best</li>
-          <li>Set out to be the best</li>
+          <li>
+            Everyday, come to our website and generate a diet and a workout
+          </li>
+          <li>
+            Follow this protocol for atleast a week and note any changes you may
+            see or feel
+          </li>
+          <li>
+            These protocols are intentionally designed to be quick and not
+            terribly taxing
+          </li>
         </ul>
       </div>
       <div className="col">
         <h2>Who is this for?</h2>
         <ul>
-          <li>Set out to be the best</li>
-          <li>Set out to be the best</li>
+          <li>
+            This is for anyone out there who wants to make a change in their
+            life
+          </li>
+          <li>From beginners to advanced, it does not matter</li>
+          <li>Consistency and habit forming is key</li>
         </ul>
       </div>
     </article>
